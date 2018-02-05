a710bf31a50f5e1201094d31e8232b22c8f78ae1
Shamelaq <qazeishamela@gmail.com>
2018-02-04 16:39:04 -0800

    first commit

---
+++ keys.js
@@ -0,0 +1,14 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<ExHhVHK3o1VdHDTnqlWhXsWAu>',
+  consumer_secret: '<H7V6ywSudAvbD7SvkrVrT3XNPj4iGnGHFCDxgBkbLb6ZPoF4o9>',
+  access_token_key: '<919718148126986240jz4PQakYUaZxfKMZEcdKMCBQZE3CTna>',
+  access_token_secret: '<PaHRKh4QqvQReLDoEJZnKTC5EyoSzytdv2ApMuNvblgxn>',
+}
+
+
+exports.spotifyKeys = {
+  clientID: '0bbfcf98a2ed4d68a546381b8fc4b648',
+  clientSecret: 'a7e26df82b164fbf87db204697049851'
+};
